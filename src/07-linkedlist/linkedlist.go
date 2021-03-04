0a08a85f9be01701f8df64728a74de07d0b2e457
lleitong <leitong@xinyu668.com>
2021-03-04 17:23:43 +0800

    07_linkedlist

--- src/07-linkedlist/linkedlist.go
+++ src/07-linkedlist/linkedlist.go
@@ -68,3 +68,70 @@ func (this *LinkedList)HasCycle() bool {
 /*
 两个单链表合并
  */
+func MergeSortedList(l1, l2 *LinkedList) *LinkedList {
+	if l1 == nil || l1.Head == nil || l1.Head.Next == nil {
+		return l2
+	}
+	if l2 == nil || l2.Head == nil || l2.Head.Next == nil {
+		return l1
+	}
+	l := &LinkedList{ Head: &ListNode{}}
+	cur := l.Head
+	cur1 := l1.Head.Next
+	cur2 := l2.Head.Next
+	for cur1 != nil && cur2 != nil {
+		if cur1.Value.(int) > cur2.Value.(int) {
+			cur.Next = cur2
+			cur2 = cur2.Next
+		} else {
+			cur.Next = cur1
+			cur1 = cur1.Next
+		}
+		cur = cur.Next
+	}
+	if cur1 != nil {
+		cur.Next = cur1
+	} else {
+		cur.Next = cur2
+	}
+	return l
+}
+
+/*
+删除倒数第N个结点
+ */
+
+func (this *LinkedList)DeleteBottomN(n int) {
+	if n <= 0 || this.Head == nil || this.Head.Next == nil {
+		return
+	}
+	cur := this.Head
+	for i := 0; i < n && cur != nil;i++ {
+		cur = cur.Next
+	}
+	if cur == nil {
+		return
+	}
+	del := this.Head
+	for cur.Next != nil {
+		cur = cur.Next
+		del = del.Next
+	}
+	del.Next = del.Next.Next
+}
+
+/*
+获取中间结点 (876)
+ */
+func (this *LinkedList)FindMiddleNode() *ListNode {
+	if this.Head == nil || this.Head.Next == nil {
+		return nil
+	}
+	fast := this.Head.Next
+	slow := this.Head.Next
+	for fast != nil && fast.Next != nil {
+		fast =fast.Next.Next
+		slow = slow.Next
+	}
+	return slow
+}
--- src/07-linkedlist/linkedlist_test.go
+++ src/07-linkedlist/linkedlist_test.go
@@ -4,7 +4,8 @@ import "testing"
 
 var l *LinkedList
 func init() {
-	n5 := &ListNode{Value: 5}
+	n6 := &ListNode{Value: 6}
+	n5 := &ListNode{Value: 5,Next: n6}
 	n4 := &ListNode{Value: 4, Next: n5}
 	n3 := &ListNode{Value: 3, Next: n4}
 	n2 := &ListNode{Value: 2, Next: n3}
@@ -21,4 +22,31 @@ func TestLinkedList_HasCycle(t *testing.T) {
 	t.Log(l.HasCycle())
 	l.Head.Next.Next.Next.Next.Next.Next = l.Head.Next.Next
 	t.Log(l.HasCycle())
+}
+
+func TestMergeSortedList(t *testing.T) {
+	n5 := &ListNode{Value: 9}
+	n4 := &ListNode{Value: 7, Next: n5}
+	n3 := &ListNode{Value: 5, Next: n4}
+	n2 := &ListNode{Value: 3, Next: n3}
+	n1 := &ListNode{Value: 1, Next: n2}
+	l1 := &LinkedList{Head: &ListNode{Next: n1}}
+	l1.Print()
+	n10 := &ListNode{Value: 10}
+	n9 := &ListNode{Value: 8, Next: n10}
+	n8 := &ListNode{Value: 6, Next: n9}
+	n7 := &ListNode{Value: 4, Next: n8}
+	n6 := &ListNode{Value: 2, Next: n7}
+	l2 := &LinkedList{Head: &ListNode{Next: n6}}
+	l2.Print()
+	MergeSortedList(l1, l2).Print()
+}
+func TestLinkedList_DeleteBottomN(t *testing.T) {
+	l.Print()
+	l.DeleteBottomN(1)
+	l.Print()
+}
+func TestLinkedList_FindMiddleNode(t *testing.T) {
+	l.Print()
+	t.Log(l.FindMiddleNode())
 }
